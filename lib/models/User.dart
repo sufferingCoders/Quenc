c05e1eb5a13dec3c974779b855a7960dbf8ec52e
Chihcheng Hsieh <mike820808@gmail.com>
2019-12-06 10:58:39 +1000

    addingLayout

---
+++ lib/models/User.dart
@@ -0,0 +1,65 @@
+class User {
+  final String displayName;
+  final String email;
+  final DateTime lastSeen;
+  final String gender;
+  final String photoURL;
+  final String uid;
+  final DateTime addedTime;
+  final String currentRoom;
+  final String major;
+  final DateTime dob;
+
+  User({
+    this.displayName,
+    this.email,
+    this.lastSeen,
+    this.photoURL,
+    this.uid,
+    this.addedTime,
+    this.currentRoom,
+    this.gender,
+    this.major,
+    this.dob,
+  });
+
+  factory User.fromMap(Map data) {
+    return User(
+      displayName: data["displayName"] ?? "",
+      email: data["email"] ?? "",
+      lastSeen: DateTime.now(),
+      photoURL: data["photoURL"] ?? "",
+      uid: data["uid"] ?? "",
+      addedTime: data["addedTime"]?.toDate() ?? DateTime.now(),
+      currentRoom: data["currentRoom"] ?? null,
+      gender: data["gender"] ?? "",
+      major: data["major"] ?? "",
+      dob: data["dob"]?.toDate() ?? DateTime.now(),
+    );
+  }
+
+  bool haveAttributesSet() {
+    List<dynamic> neededField = [gender, major, dob];
+    for (var ele in neededField) {
+      if (ele == null || ele.toString().isEmpty) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      "displayName": displayName,
+      "email": email,
+      "lastSeen": lastSeen,
+      "photoURL": photoURL,
+      "uid": uid,
+      "addedTime": addedTime,
+      "currentRoom": currentRoom,
+      "gender": gender,
+      "major": major,
+      "dob": dob,
+    };
+  }
+}
---
+++ lib/providers/UserService.dart
@@ -0,0 +1,66 @@
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:quenc/models/User.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class UserService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final Firestore _db = Firestore.instance;
+
+  void updateFirebaseUserDataToDB(FirebaseUser user) async {
+    DocumentReference ref = _db.collection('users').document(user.uid);
+
+    return ref.setData({
+      'uid': user.uid,
+      'email': user.email,
+      'photoURL': user.photoUrl,
+      'displayName': user.displayName,
+      'lastSeen': DateTime.now(),
+    }, merge: true);
+  }
+
+  Stream<User> userStream(FirebaseUser user) {
+    print("User stream is called");
+    if (user == null) return null;
+    print("User stream is called and return stream");
+
+    return _db
+        .collection("users")
+        .document(user.uid)
+        .snapshots()
+        .map((u) => User.fromMap(u.data));
+  }
+
+  Future<FirebaseUser> loginWithEmailAndPassword(
+      String email, String password) async {
+    try {
+      FirebaseUser user = (await _auth.signInWithEmailAndPassword(
+              email: email, password: password))
+          .user;
+
+      updateFirebaseUserDataToDB(user);
+      final prefs = await SharedPreferences.getInstance();
+      prefs.setString("email", json.encode(email));
+      prefs.setString("password", password);
+      // Save emeail and password in prefernece
+      return user;
+    } catch (error) {
+      print("The error is ${error.toString()}");
+      return null;
+    }
+  }
+
+  Future<bool> tryAutoLogin() async {
+    final prefs = await SharedPreferences.getInstance();
+    if (!prefs.containsKey("email") || !prefs.containsKey("password")) {
+      return false;
+    }
+
+    String email = prefs.getString("email");
+    String password = prefs.getString("password");
+    loginWithEmailAndPassword(email, password);
+    return true;
+  }
+}
